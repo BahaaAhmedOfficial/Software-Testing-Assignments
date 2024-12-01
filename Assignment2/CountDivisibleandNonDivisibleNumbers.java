ec52c5dd9ae208685c0c2817579593cb1f98758b
Bahaa Ahmed <BahaaAhmedOfficial@gmail.com>
2024-12-01 15:40:48 +0200

    update

--- Assignment2/CountDivisibleandNonDivisibleNumbers.java
+++ Assignment2/CountDivisibleandNonDivisibleNumbers.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class CountDivisibleandNonDivisibleNumbers {
     public static void main(String[] args) {
-        // We init the vairables to be used
+        // We init the variables to be used
 
-        int size, divisor, divisibleCount, nonDivisbleCount, i;
-        size = divisor = divisibleCount = nonDivisbleCount = i = 0;
+        int size, divisor, divisibleCount, nonDivisibleCount, i;
+        size = divisor = divisibleCount = nonDivisibleCount = i = 0;
 
         Scanner input = new Scanner(System.in);
 
@@ -28,27 +28,29 @@ public class CountDivisibleandNonDivisibleNumbers {
         System.out.print("Enter a divisor: ");
         divisor = input.nextInt();
 
-        // Do-While loop checks the divisiblity
+        // Do-While loop checks the divisibility
         i = 0;
         do {
             if (numbers[i] % divisor == 0) {
                 divisibleNumbers[divisibleCount] = numbers[i];
                 divisibleCount++;
             } else
-                nonDivisbleCount++;
+                nonDivisibleCount++;
 
             i++;
         } while (i < size);
 
-        // Print statments to showcase the results
-        System.out.println("The number of non-divisble numbers are " + nonDivisbleCount);
-        System.out.println("The number of divisble numbers are " + divisibleCount);
+        // Print statements to showcase the results
+        System.out.println("The number of non-divisible numbers are " + nonDivisibleCount);
+        System.out.println("The number of divisible numbers are " + divisibleCount);
 
         // Do-While loop to show the divisible numbers
         i = 0;
         do {
-            System.out.println("The divisble numbers are " + divisibleNumbers[i]);
+            System.out.println("The divisible numbers are " + divisibleNumbers[i]);
             i++;
         } while (i < divisibleCount);
+
+        input.close();
     }
 }
